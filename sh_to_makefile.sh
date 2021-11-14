cb7f340d57168340b5a588195f4e8ef66751b711
peanutbuttermurmite <73216267+peanutbuttermurmite@users.noreply.github.com>
2021-11-14 15:09:51 +0000

    Update sh_to_makefile.sh
    Removed Unnecessary code and cleaned up

--- sh_to_makefile.sh
+++ sh_to_makefile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-GREEN='\33[1;32m'
-read -p "Enter the directory of the shell script: "  shscript
+read -p "Enter the directory of the shell script or the relative directory: "  shscript
 read -p "Enter the target directory for your Makefile: " targetdir
 read -p "Enter the name of the make command you would like to invoke the script: " makeinvoke 
 cd $targetdir
@@ -13,5 +12,5 @@ cat $shscript >> Makefile
 printf 'endef' >> Makefile
 printf ${makeinvoke3} >> Makefile
 printf '.ONESHELL:' >> Makefile
-printf '${GREEN}Shell script successfully converted to Makefile'
+printf 'Shell script successfully converted to Makefile'
 
